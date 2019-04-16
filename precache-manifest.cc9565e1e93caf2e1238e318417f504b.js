4980b3b53195c8756f21abaa0885d28a2cfae2e2
Vitor Paes <paesvitor@outlook.com>
2019-04-16 13:02:14 -0300

    Updates

---
+++ precache-manifest.cc9565e1e93caf2e1238e318417f504b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1c8122caac6847ef9f73e6ce03e42c3",
+    "url": "/desincha/static/media/video_thumbnail@2x.b1c8122c.png"
+  },
+  {
+    "revision": "7652a78a56dc0d8d69e2",
+    "url": "/desincha/static/js/main.1909f703.chunk.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/desincha/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/desincha/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "543311a296c2c9ed75af070bb37e6449",
+    "url": "/desincha/static/media/smoothie@2x.543311a2.png"
+  },
+  {
+    "revision": "4abe966ed0102b43ff2bcc15bcf995a8",
+    "url": "/desincha/static/media/product_1.4abe966e.png"
+  },
+  {
+    "revision": "e4cb0d756cc742dee75d378a0abdaa23",
+    "url": "/desincha/static/media/product_2@2x.e4cb0d75.png"
+  },
+  {
+    "revision": "9fd31e50eba9692a5cbd9fb9c6306ca5",
+    "url": "/desincha/static/media/ingredients_header@2x.9fd31e50.png"
+  },
+  {
+    "revision": "a83c1cc5f38316f00cb6bb07933d3412",
+    "url": "/desincha/static/media/ingredients@2x.a83c1cc5.png"
+  },
+  {
+    "revision": "f1683795ec930dbba826",
+    "url": "/desincha/static/js/runtime~main.0f7d93f3.js"
+  },
+  {
+    "revision": "9bc1c7300212121de0ebf9308202bcee",
+    "url": "/desincha/static/media/blog_1@2x.9bc1c730.png"
+  },
+  {
+    "revision": "798d0d1c9a0a2d31c9b48f09435ce348",
+    "url": "/desincha/static/media/blog_2@2x.798d0d1c.png"
+  },
+  {
+    "revision": "c9ad4af525e8f2572064e95c45040d8d",
+    "url": "/desincha/static/media/intro.c9ad4af5.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/desincha/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/desincha/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "9763e86660e2f75d7960",
+    "url": "/desincha/static/js/2.7c5585b7.chunk.js"
+  },
+  {
+    "revision": "9763e86660e2f75d7960",
+    "url": "/desincha/static/css/2.04ef47c4.chunk.css"
+  },
+  {
+    "revision": "c34aceffd28dbdae5d26e3bf675d1aec",
+    "url": "/desincha/index.html"
+  }
+];
